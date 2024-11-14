f9b61163d473a25881d40b9aed8c58fb77a9f830
IainMesser <iain.messer@edu.sait.ca>
2024-11-14 10:51:11 -0700

    Pop up

--- Js/Mainpagebackend.js
+++ Js/Mainpagebackend.js
@@ -1,3 +1,25 @@
+// Modal pop up with text
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("closeModal");
+
+// Check if the modal has already been shown
+if (!localStorage.getItem("modalShown")) {
+    window.addEventListener("load", function() {
+      modal.style.display = "flex";
+    });
+    localStorage.setItem("modalShown", "true");
+  }
+
+closeModal.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 
 
@@ -6,3 +28,4 @@
 const fYear = document.getElementById("footerYear");
 const d = new Date();
 fYear.textContent = d.getFullYear();
+console.log(d.toLocaleString());
