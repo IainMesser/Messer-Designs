3c2dd16a8e75f9091b2f302b1d2f4a8c0f1f201d
IainMesser <iain.messer@edu.sait.ca>
2024-11-15 10:23:16 -0700

    Add pop up message when btn hit

--- Js/Mainpagebackend.js
+++ Js/Mainpagebackend.js
@@ -20,6 +20,37 @@ window.onclick = function(event) {
 
 
 
+//Greatings deepending on the time
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+    const d = new Date();
+    const hour = d.getHours();
+    let message = "";
+    if (hour < 12) {
+        message = "Good morning!";
+    } else if (hour < 18) {
+        message = "Good afternoon!";
+    } else {
+        message = "Good evening!";
+    }
+
+    // Create a div for the message
+    const messageDiv = document.createElement("div");
+    messageDiv.classList.add("popup-message"); 
+    messageDiv.textContent = message;
+    document.body.appendChild(messageDiv);
+    setTimeout(() => {
+        messageDiv.classList.add("fade-out");
+        setTimeout(() => {
+            messageDiv.remove(); 
+        }, 500); 
+    }, 3000); 
+});
+
+
+
+
+
 //Footer Year 
 const fYear = document.getElementById("footerYear");
 const d = new Date();
